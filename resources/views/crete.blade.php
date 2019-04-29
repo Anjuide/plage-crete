41e6d60e777aa741e16a66571046eb8172aff577
Anjuide <ange.gnamba@gmail.com>
2019-04-29 21:30:16 +0200

    Initialisation des fichiers

---
+++ resources/views/crete.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Les plus belles plages de Crète</title>
+</head>
+
+<body>
+    <h1>Les plages en Crète</h1>
+    <span>Trier par:</span>
+    <select name="tri" id="tri">
+        <option value="nom">Nom de plage de A à Z</option>
+    </select>
+    <div>
+        <h2>Plage d’Elafonissi</h2>
+        <h3>Description</h3>
+        <p>Description de la plage</p>
+        <img src="https://photos.smugmug.com/Travel/Greece/5e-Elafosini/i-txk3VXz/2/8e1c3d5c/L/Elafonisi-beach-crete-greece-4943-L.jpg" alt="Image d'une plage de Crête">
+        <h3>Détails</h3>
+        <p>Accès:</p>
+        <p>Installations:</p>
+        <p>Sable:</p>
+        <p>Affluence touristes:</p>
+    </div>
+    <div>
+        <h2>Plage de Balos Beach</h2>
+        <h3>Description</h3>
+        <p>Description de la plage</p>
+        <img src="https://photos.smugmug.com/Travel/Greece/5e-Elafosini/i-txk3VXz/2/8e1c3d5c/L/Elafonisi-beach-crete-greece-4943-L.jpg" alt="Image d'une plage de Crête">
+        <h3>Détails</h3>
+        <p>Accès:</p>
+        <p>Installations:</p>
+        <p>Sable:</p>
+        <p>Affluence touristes:</p>
+    </div>
+    <div>
+        <h2>Plage Red Beach</h2>
+        <h3>Description</h3>
+        <p>Description de la plage</p>
+        <img src="https://photos.smugmug.com/Travel/Greece/5e-Elafosini/i-txk3VXz/2/8e1c3d5c/L/Elafonisi-beach-crete-greece-4943-L.jpg" alt="Image d'une plage de Crête">
+        <h3>Détails</h3>
+        <p>Accès:</p>
+        <p>Installations:</p>
+        <p>Sable:</p>
+        <p>Affluence touristes:</p>
+    </div>
+    <div>
+        <h2>Plage de Sfinari</h2>
+        <h3>Description</h3>
+        <p>Description de la plage</p>
+        <img src="https://photos.smugmug.com/Travel/Greece/5e-Elafosini/i-txk3VXz/2/8e1c3d5c/L/Elafonisi-beach-crete-greece-4943-L.jpg" alt="Image d'une plage de Crête">
+        <h3>Détails</h3>
+        <p>Accès:</p>
+        <p>Installations:</p>
+        <p>Sable:</p>
+        <p>Affluence touristes:</p>
+    </div>
+    <div>
+        <h2>Plage de Vaî</h2>
+        <h3>Description</h3>
+        <p>Description de la plage</p>
+        <img src="https://photos.smugmug.com/Travel/Greece/5e-Elafosini/i-txk3VXz/2/8e1c3d5c/L/Elafonisi-beach-crete-greece-4943-L.jpg" alt="Image d'une plage de Crête">
+        <h3>Détails</h3>
+        <p>Accès:</p>
+        <p>Installations:</p>
+        <p>Sable:</p>
+        <p>Affluence touristes:</p>
+    </div>
+    <div>
+        <h2>Plages de Phalassarna</h2>
+        <h3>Description</h3>
+        <p>Description de la plage</p>
+        <img src="https://photos.smugmug.com/Travel/Greece/5e-Elafosini/i-txk3VXz/2/8e1c3d5c/L/Elafonisi-beach-crete-greece-4943-L.jpg" alt="Image d'une plage de Crête">
+        <h3>Détails</h3>
+        <p>Accès:</p>
+        <p>Installations:</p>
+        <p>Sable:</p>
+        <p>Affluence touristes:</p>
+    </div>
+
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Page d'accueil
 Route::get('/', function () {
     return view('welcome');
+});
+// Liste des plages de la Crète
+Route::get('/plages-crete', function () {
+    return view('crete');
+});
+
+Route::get('/plages-cote-d-ivoire', function () {
+    return view('ivorycoast');
 });
